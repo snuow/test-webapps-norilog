a639af9f9dc62349f13581cd0747195fc6390bd4
snuow <24.nishi.yuuki@gmail.com>
2020-01-15 22:01:21 +0900

    20200115 mv index.html fix norilog.py

--- norilog.py
+++ norilog.py
@@ -1,4 +1,5 @@
 import json
+from flask import Flask,render_template
 
 DATA_FILE = 'norilog.json'
 
@@ -24,3 +25,14 @@ def save_data(start,finish,memo,create_at):
     })
 
     json.dump(database,open(DATA_FILE,mode="w",encoding="utf-8"),indent=4,ensure_ascii=False)
+
+
+def load_data():
+    try:
+        # jsonモジュールでデータベースファイルを開きます
+        database = json.load(open(DATA_FILE,mode="r",encoding="utf-8"))
+    except FileNotFoundError:
+        database = []
+
+    return database
+
